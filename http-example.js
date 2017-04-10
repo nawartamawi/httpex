d40867d56c338e995207687cae7d431a23303a92
nawartamawi <nawartamawi@gmail.com>
2017-04-10 05:51:43 +0000

    step 7 bigger data response

--- http-example.js
+++ http-example.js
@@ -1,8 +1,8 @@
 var https = require('https');
 console.log('I did it!');
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 var callback = function(response) {
   console.log('In response handler callback!');
